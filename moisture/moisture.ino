496af3bdab3c1d2eb9952f65688ed29c7bfbd3c9
PieterWinderlichSchool <pieterwinderlich@outlook.com>
2018-10-09 14:43:17 +0200

    addes pictures of echo

---
+++ moisture/moisture.ino
@@ -0,0 +1,27 @@
+//https://www.electroschematics.com/6519/simple-soil-moisture-sensor-arduino-project/
+
+
+const int VAL_PROBE = 0; // Analog pin 0
+const int MOISTURE_LEVEL = 250; // the value after the LED goes ON
+ 
+void setup() {
+    Serial.begin(9600);
+}
+ 
+void LedState(int state) {
+    digitalWrite(13, state);
+}
+ 
+void loop() {
+    int moisture = analogRead(VAL_PROBE);
+    moisture = map(moisture, 1023, 0, 0, 1023);
+    Serial.println(moisture);
+ 
+    if(moisture > MOISTURE_LEVEL) {
+        LedState(HIGH);
+    } else   {
+        LedState(LOW);
+    }
+    
+    delay(100);
+}
